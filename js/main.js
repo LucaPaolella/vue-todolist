47e023d077009430365d1588d5d6da95d4af880f
Your Name <you@example.com>
2023-03-15 16:43:06 +0100

    milestone 0

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            todo:{
+                spesa:{
+                    text:"fare spesa",
+                    done: true
+                },
+
+                nutrireGatto:{
+                    text:"fare spesa",
+                    done: false
+                },
+
+                lavareMacchina:{
+                    text:"fare spesa",
+                    done: true
+                },
+            }
+        }
+
+    }
+}).mount("#app");
